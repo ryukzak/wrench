f42b337fb6de992f9a46df4ed3905dfa2af9109c
Kirill Zakusov <username918r818@outlook.com>
2025-02-22 01:03:55 +0300

    overflow check using division (requires significantly more instruction limit)
    more stack state comments

--- test/golden/f32a/factorial.s
+++ test/golden/f32a/factorial.s
@@ -2,6 +2,8 @@
 
 input_addr:      .word  0x80
 output_addr:     .word  0x84
+tmp_divisor:     .word  0x0
+tmp_prev:        .word  0x1
 alligment:       .word  '................................'
 
     .text
@@ -9,6 +11,8 @@ alligment:       .word  '................................'
     \\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\
 
 _start:
+    lit tmp_divisor
+    b!
     @p input_addr a! @       \ n:[]
     
     dup
@@ -49,6 +53,16 @@ multiply_do:
 
     \\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\
 
+divide:
+    lit 31 r>                \ for R = 31
+divide_do:
+    +/
+
+    next divide_do
+    ;
+
+    \\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\
+
 factorial:
     lit 1 over               \ n:acc:[]
 factorial_while:
@@ -58,18 +72,31 @@ factorial_while:
     dup a!                   \ n:acc:[]
 
     over                     \ acc:n:[]
-    dup
-    -if continue_factorial
-    handler_overflow
+    dup !p tmp_prev
 
 continue_factorial:
     lit 0                    \ 0:acc:n:[]
 
-    multiply
+    multiply                 \ acc:n:[]
 
-    over                     \ n:acc
+    a! !b                    \ []
+    a @b                     \ n:acc:[]
+    lit 0 lit 0              \ 0:0:n:acc:[]
+    divide                   \ quot:rem:n:acc:[]
+    over                     \ rem:quot:n:acc:[]
+    if continue_factorial2
+    handler_overflow
+
+continue_factorial2:
+    @p tmp_prev              \ prev_acc:quot:n:acc[]
+    
+    inv lit 1 + +            \ quot-prev_acc:n:acc[]
+
+    if continue_factorial3
+    handler_overflow
+
+continue_factorial3:
     lit -1 +                 \ n-1:acc
-
     factorial_while ;
 
 factorial_finish:
