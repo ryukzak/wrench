70600df2360cee6762c1e6f330e981f472279a0e
Aleksandr Penskoi <aleksandr.penskoi@gmail.com>
2025-03-02 01:31:39 +0100

    Remove quote from config name during variant generation.

--- script/variants.py
+++ script/variants.py
@@ -115,7 +115,7 @@ def run_python_test_cases(verbose):
 def generate_wrench_test_cases(conf_name, case):
     limit = 2000
     conf_name = case.assert_string(conf_name)
-    return f"""name: "{conf_name}"
+    return f"""name: {conf_name}
 limit: {limit}
 memory_size: 0x1000
 input_streams:
