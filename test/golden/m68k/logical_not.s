e0739c4da4124a42ee45a03a0811252352c28587
Aleksandr Penskoi <aleksandr.penskoi@gmail.com>
2025-04-27 19:18:12 +0200

    Add condition branch instructions and get_put_char alg.

--- test/golden/m68k/logical_not.s
+++ test/golden/m68k/logical_not.s
@@ -7,11 +7,15 @@ output_addr:     .word  0x84               ; Output address where the result sho
 
 _start:
     movea.l  input_addr, A0                  ; A0 <- input_addr
+    movea.l  (A0), A0
     move.l   (A0), D0                        ; D0 <- n (input value)
 
     not.l    D0                              ; D0 <- ~D0 (logical NOT)
+    move.l   0x01, D1
+    and.l    D1, D0
 
     movea.l  output_addr, A1                 ; A1 <- output_addr
+    movea.l  (A1), A1
     move.l   D0, (A1)                        ; *output_addr <- D0 (result)
 
     halt
