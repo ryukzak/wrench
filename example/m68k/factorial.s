1303248472222d1af5e578d9baf9f3e861f6ac77
Aleksandr Penskoi <aleksandr.penskoi@gmail.com>
2025-05-18 00:42:02 +0200

    Add factorial example

--- example/m68k/factorial.s
+++ example/m68k/factorial.s
@@ -6,21 +6,21 @@ output_addr:     .word  0x84               ; Output address where the result sho
     .text
 
 _start:
-    move.l   input_addr, A0                  ; A0 <- input_addr
-    move.l   (A0), A0                        ; A0 <- *input_addr
+    movea.l  input_addr, A6
+    movea.l  (A6), A6
+    movea.l  output_addr, A7
+    movea.l  (A7), A7
 
-    move.l   (A0), D0                        ; D0 <- n (input value)
-    moveq    #1, D1                          ; D1 <- 1 (accumulator)
+    move.l   (A6), D0
+    move.l   D0, D1
 
 factorial_while:
-    beq      factorial_end                   ; if (D0 == 0) goto factorial_end
-    muls     D0, D1                          ; D1 <- D1 * D0
-    subq.l   #1, D0                          ; D0 <- D0 - 1
-    bra      factorial_while                 ; loop back to factorial_while
+    sub.l    1, D1
+    beq      factorial_end
+    mul.l    D1, D0
+    jmp      factorial_while
 
 factorial_end:
-    move.l   output_addr, A1                 ; A1 <- output_addr
-    move.l   (A1), A1                        ; A1 <- *output_addr
-    move.l   D1, (A1)                        ; *output_addr <- D1 (result)
+    move.l   D0, (A7)
 
     halt
