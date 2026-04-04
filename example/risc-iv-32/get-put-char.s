609128506cb094b97ec09ea7c9aa5170818ff543
Lev Simonov <102740544+ExtraCat507@users.noreply.github.com>
2026-04-04 15:50:41 +0300

    fix: typo output_addr instead of input_addr

--- example/risc-iv-32/get-put-char.s
+++ example/risc-iv-32/get-put-char.s
@@ -10,7 +10,7 @@ _start:
     lui      t0, %hi(input_addr)             ; int * input_addr_const = *input_addr;
     addi     t0, t0, %lo(input_addr)
 
-    lw       t0, 0(t0)                       ; int output_addr = *output_addr_const;
+    lw       t0, 0(t0)                       ; int input_addr = *input_addr_const;
 
     lui      t1, %hi(output_addr)            ; int * output_addr_const = *output_addr;
     addi     t1, t1, %lo(output_addr)
