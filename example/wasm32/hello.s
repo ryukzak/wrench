acbe3d879cc3ea895e4cd4b7f590e0757a40c3f4
Gusarich <danielsedovzzz@gmail.com>
2026-05-25 22:48:37 +0300

    Add Wasm32 ISA

---
+++ example/wasm32/hello.s
@@ -0,0 +1,37 @@
+    .data
+
+buf:             .byte  31, 'H' , 'e' , 'l' , 'l' , 'o' , '\n' , '\0' , 'W' , 'o' , 'r' , 'l' , 'd' , '!'
+buf_end:         .byte  0, 0, 0
+
+    .text
+
+_start:
+    .func    locals $ptr $end
+    i32.const buf
+    local.set $ptr
+    i32.const buf_end
+    local.set $end
+
+    block    $done
+    loop     $loop
+    local.get $ptr
+    local.get $end
+    i32.ge_u
+    br_if    $done
+
+    i32.const 0x84
+    local.get $ptr
+    i32.load8_u
+    i32.store8
+
+    local.get $ptr
+    i32.const 1
+    i32.add
+    local.set $ptr
+
+    br       $loop
+    end
+    end
+
+    halt
+    .endfunc
